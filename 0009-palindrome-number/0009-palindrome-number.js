346763ad9a9422b04ea17b289dd9cbcb85c93103
shifinshalu035-cell <shifinshalu035@gmail.com>
2026-07-07 10:25:14 +0530

    Time: 3 ms (98.14%), Space: 62.3 MB (97.9%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if(x<0){
+        return false
+    }
+    let reverse = 0;
+    let og = x;
+    while(x>0){
+        let digit = x % 10;
+        reverse = reverse * 10 + digit;
+        x=Math.floor(x/10);
+        
+    }
+    return og === reverse;
+
+    
+    
+};
