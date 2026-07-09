bff1ca389ab91b69a9b376060d1d6aa7623bf27f
shifinshalu035-cell <shifinshalu035@gmail.com>
2026-07-09 10:20:44 +0530

    Time: 2 ms (73.03%), Space: 55.5 MB (46.07%) - LeetHub

---
+++ 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var countEven = function(num) {
+       let count = 0;
+       for(let i=1;i<=num;i++){
+        let sum = 0;
+        let temp = i;
+        while(temp > 0){
+            sum += temp % 10;
+            temp = Math.floor(temp/10);
+        }
+        if(sum % 2 === 0){
+            count++;
+        }
+       }
+        return count;
+    };
+
+ 
+    
+
+
