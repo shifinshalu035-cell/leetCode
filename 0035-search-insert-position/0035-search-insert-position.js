89908345dcef8082fb8390fc08b26e353ba28662
shifinshalu035-cell <shifinshalu035@gmail.com>
2026-07-24 10:26:26 +0530

    Time: 0 ms (100%), Space: 52.8 MB (93.6%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -4,26 +4,20 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-
-    let left = 0;
-    let right = nums.length - 1;
-
+    let left = 0
+    let right = nums.length-1
     while(left <= right){
-
-        let mid = Math.floor((left + right) / 2);
-
-        if(target === nums[mid]){
+        let mid = Math.floor((left+right)/2)
+        if(nums[mid] === target){
             return mid;
         }
-
-        else if(target < nums[mid]){
-            right = mid - 1;
+        if(nums[mid] < target ){
+            left = mid + 1
         }
-
         else{
-            left = mid + 1;
+            right = mid - 1
         }
     }
-
     return left;
+    
 };
